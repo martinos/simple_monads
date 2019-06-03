0c10dfa0d8a44998df787c384f528f3fe8b54014
Martin Chabot <chabotm@gmail.com>
2019-06-03 09:43:02 -0400

    fixing result monad

--- lib/result.ex
+++ lib/result.ex
@@ -1,8 +1,23 @@
 defmodule Result do
-  def and_then({:error, result}, _), do: {:error, result}
-  def and_then({:ok, val}, fnc), do: {:ok, fnc.(val)}
+  def map({:error, result}, _), do: {:error, result}
+  def map({:ok, val}, fnc), do: {:ok, fnc.(val)}
+  def and_then({:error, result}, _fnc), do: {:error, result}
+
+  def and_then({:ok, result}, fnc) do
+    case fnc.(result) do
+      {:ok, result} ->
+        {:ok, result}
+
+      {:error, result} ->
+        {:error, result}
+
+      result ->
+        raise "Invalid return value #{result} for the function passed to the and_then Result"
+    end
+  end
+
   def map_error(result = {:ok, _}, _), do: result
-  def map_error({:error, val}, fnc) when is_function(fnc), do: {:error, fnc.(val) }
+  def map_error({:error, val}, fnc) when is_function(fnc), do: {:error, fnc.(val)}
   def map_error({:error, _}, new_error), do: {:error, new_error}
 
   def from_maybe(nil, error), do: {:error, error}
--- test/result_test.exs
+++ test/result_test.exs
@@ -2,36 +2,40 @@ defmodule ResultTest do
   use ExUnit.Case
   import Result
 
-  test "and_then returns the error when the input is an error" do
-    assert ({:error, "My Error"} |> and_then(fn(_) -> 2 end)) == {:error, "My Error"}
+  test "map returns the error when the input is an error" do
+    assert {:error, "My Error"} |> map(fn _ -> 2 end) == {:error, "My Error"}
   end
 
-  test "and_then returns the value processed by the given function when the input value a success" do
-    assert ({:ok, 2} |> and_then(fn(a) -> a + 1 end)) == {:ok, 3}
+  test "map returns the value processed by the given function when the input value a success" do
+    assert {:ok, 2} |> map(fn a -> a + 1 end) == {:ok, 3}
+  end
+
+  test "and_then should return the first result if first one error" do
+    assert {:error, "My error"} |> and_then(fn _ -> {:ok, 2} end) == {:error, "My error"}
+  end
+
+  test "and_then should return ok if first result is ok and the return value returned by the lamda is :ok" do
+    assert {:ok, 1} |> and_then(fn _ -> {:ok, 2} end) == {:ok, 2}
   end
 
   test "map the error when there is an error as input" do
-    assert ({:error, "This is an error"} |> map_error("New error")) == {:error, "New error"}
+    assert {:error, "This is an error"} |> map_error("New error") == {:error, "New error"}
   end
 
   test "map the error with a function when there is an error as input" do
-    assert ({:error, "This is an error"} |> map_error(fn(a) -> a <> "2" end)) == {:error, "This is an error2"}
+    assert {:error, "This is an error"} |> map_error(fn a -> a <> "2" end) ==
+             {:error, "This is an error2"}
   end
 
   test "return the input when success on map_error" do
-    assert ({:ok, 12} |> map_error("New error")) == {:ok, 12}
-  end
-
-  test "and_then returns a success when ok is sent to and_then" do
-    assert ({:ok, 1} |> and_then(fn(a) -> a + 2 end)) == {:ok, 3}
+    assert {:ok, 12} |> map_error("New error") == {:ok, 12}
   end
 
   test "converts to an error result when nil value" do
-    assert (nil |> from_maybe("My Error")) ==  {:error, "My Error"}
+    assert nil |> from_maybe("My Error") == {:error, "My Error"}
   end
 
   test "converts to an success result when non nil value" do
-    assert (12 |> from_maybe("My Error")) ==  {:ok, 12}
+    assert 12 |> from_maybe("My Error") == {:ok, 12}
   end
 end
-
