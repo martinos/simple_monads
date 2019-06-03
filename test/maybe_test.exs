9454ad186796e2b7c2d1d027bad05efd702c4440
Martin Chabot <chabotm@gmail.com>
2019-06-03 10:03:56 -0400

    fixing result monad

--- test/maybe_test.exs
+++ test/maybe_test.exs
@@ -3,18 +3,18 @@ defmodule MaybeTest do
   import Maybe
 
   test "and_then should return nil if nil is passed as first arg" do
-    refute nil |> and_then(fn -> 3 end) 
+    refute nil |> map(fn -> 3 end)
   end
 
   test "passes the first args to the function and return the returned val" do
-    assert (4 |> and_then(fn(a) -> a + 8 end)) == 12
+    assert 4 |> map(fn a -> a + 8 end) == 12
   end
 
   test "sets the a default value when one result in the chain is nil" do
-    assert (4 |> and_then(fn(_) -> nil end)) |> with_default(33) == 33 
+    assert 4 |> map(fn _ -> nil end) |> with_default(33) == 33
   end
 
   test "sets to the default value the chain returns a val" do
-    assert (4 |> and_then(fn(_) -> 44 end)) |> with_default(33) == 44 
+    assert 4 |> map(fn _ -> 44 end) |> with_default(33) == 44
   end
 end
